150701194e6c9b07295ff6cefe143f0144103db1
EJYS <eunice.sjy@gmail.com>
2026-09-01 15:47:42 +0800

    [EJYS] Amend to show only flower prediction

--- exercise-2.py
+++ exercise-2.py
@@ -8,45 +8,60 @@ app = marimo.App(width="medium")
 def _():
     from transformers import AutoModelForImageClassification, AutoImageProcessor
     from PIL import Image
+    from IPython.display import display
 
     import torch as torch
 
-    return AutoImageProcessor, AutoModelForImageClassification, Image, torch
+    return (
+        AutoImageProcessor,
+        AutoModelForImageClassification,
+        Image,
+        display,
+        torch,
+    )
+
+
+@app.cell
+def _():
+    # Paths
+    path_img = 'img/'
+    return (path_img,)
 
 
 @app.cell
 def _(AutoImageProcessor, AutoModelForImageClassification):
-    # Variables
-    file_img = 'Welwitschia mirabilis.jpg'
-
-    # Paths
-    path_img = 'img/'
-
     # Load the model and processor from Hugging Face
     model_id = "phenobase/phenovision"
     processor = AutoImageProcessor.from_pretrained(model_id)
     model = AutoModelForImageClassification.from_pretrained(model_id)
-    return file_img, model, path_img, processor
+    return model, processor
 
 
 @app.cell
-def _(Image, file_img, path_img):
+def _(Image, display, model, path_img, processor, torch):
     # Open a local image of a plant specimen
+    # true positive: 'BM000521866.jpg', 'bad.jpg'
+    # true negative: '270005.jpg' 
+    # false positive: '196181.jpg'
+    # false negative: '269465.jpg'
+
+    file_img = 'bad.jpg' 
     image = Image.open(path_img + file_img)
-    return (image,)
+    display(image, height= 300)
 
-
-@app.cell
-def _(image, model, processor, torch):
     # Prepare inputs and run inference
     inputs = processor(images=image, return_tensors="pt")
     outputs = model(**inputs)
     probs = torch.sigmoid(outputs.logits)[0]
-    # IMPORTANT: output order is [fruit, flower] — index 0 is FRUIT, index 1 is FLOWER.
+
     fruit_prob  = probs[0].item()
     flower_prob = probs[1].item()
     print(f"Flower: {flower_prob:.3f}")
-    print(f"Fruit:  {fruit_prob:.3f}")
+    # print(f"Fruit:  {fruit_prob:.3f}")
+
+    # test on weird plants with flowers:
+    # can detect flower:  'Welwitschia mirabilis.jpg', 'Pleurothallis-truncata.jpg'
+    # cannot detect flower: 'Picture2.jpg'
     return
 
 
